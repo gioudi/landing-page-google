71f99231a4bb2e84a5d4b19f3857c6ee057ca9bc
Semi-dios <tipouno@misena.edu.co>
2022-12-13 17:05:25 -0500

    feat-bulma

---
+++ src/components/CustomNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nav
+      :class="isdark ? 'custom-navbar__bg-dark' : 'custom-navbar__bg-light'"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="is-flex-touch">
+        <span class="material-icons" @click="openSidebar()">
+          menu
+        </span>
+      </div>
+    </nav>
+
+    <div class="is-flex-touch">
+      <custom-sidebar></custom-sidebar>
+    </div>
+  </div>
+</template>
+<script>
+import CustomSidebar from './CustomSidebar.vue'
+export default {
+  name: 'CustoNav',
+  components: {
+    CustomSidebar
+  },
+  data () {
+    return {
+      isdark: false
+    }
+  }
+}
+</script>
+<style lang="scss">
+.custom-nav {
+  @media screen and (max-width: 992px) {
+    &__burger {
+      margin-right: auto;
+    }
+  }
+}
+</style>
---
+++ src/components/CustomSidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="isdark ? 'custom-sidebar__bg-dark' : 'custom-sidebar__bg-light'">
+    <div
+      :class="
+        isdark
+          ? 'custom-sidebar__content-bg-dark'
+          : 'custom-sidebar__content-bg-light'
+      "
+      class="custom-sidebar__content"
+    >
+      <div class="">
+        <picture class="is-flex">
+          <img
+            src="@/assets/img/googlelogo.png"
+            :alt="`$t('alt_image_logo')`"
+          />
+        </picture>
+        <div
+          v-for="option in options"
+          :key="option"
+          class="custom-sidebar__content-item  is-flex is-align-items-center"
+        >
+          <span class="material-icons">
+            {{ $t(`sidebar.option_${option}.icon`) }}
+          </span>
+          <p>{{ $t(`sidebar.option_${option}.text`) }}</p>
+        </div>
+      </div>
+      <div class=""></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CustomSidebar',
+  data () {
+    return {
+      isdark: false,
+      options: 11
+    }
+  }
+}
+</script>
