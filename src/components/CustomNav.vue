62dbe35ca993ce35cf760fd802ee2224875f8e10
Semi-dios <tipouno@misena.edu.co>
2022-12-14 15:48:15 -0500

    fix-sidebar

--- src/components/CustomNav.vue
+++ src/components/CustomNav.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <nav
-      :class="isdark ? 'custom-navbar__bg-dark' : 'custom-navbar__bg-light'"
+      :class="
+        backgroundDark ? 'custom-navbar__bg-dark' : 'custom-navbar__bg-light'
+      "
       role="navigation"
       aria-label="main navigation"
     >
-      <div class="is-flex-touch">
-        <span class="material-icons" @click="openSidebar()">
+      <div
+        class="is-flex-touch"
+        type="button"
+        @click="openSidebar()"
+        @touchstart="openSidebar()"
+      >
+        <span class="material-icons">
           menu
         </span>
       </div>
@@ -19,6 +26,7 @@
 </template>
 <script>
 import CustomSidebar from './CustomSidebar.vue'
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'CustoNav',
   components: {
@@ -26,7 +34,18 @@ export default {
   },
   data () {
     return {
-      isdark: false
+      isdark: false,
+      isOpenSidebar: false
+    }
+  },
+  computed: {
+    ...mapState(['backgroundDark'])
+  },
+  methods: {
+    ...mapMutations(['SET_OPEN_SIDEBAR']),
+    openSidebar () {
+      this.isOpenSidebar = !this.isOpenSidebar
+      this.SET_OPEN_SIDEBAR(this.isOpenSidebar)
     }
   }
 }
--- src/components/CustomSidebar.vue
+++ src/components/CustomSidebar.vue
@@ -1,42 +1,67 @@
 <template>
-  <div :class="isdark ? 'custom-sidebar__bg-dark' : 'custom-sidebar__bg-light'">
+  <div
+    @touchstart="touchSidebar()"
+    @click="touchSidebar()"
+    :class="[
+      backgroundDark ? 'custom-sidebar__bg-dark' : 'custom-sidebar__bg-light',
+      openSidebar ? 'is-block' : 'is-hidden'
+    ]"
+  >
     <div
       :class="
-        isdark
+        backgroundDark
           ? 'custom-sidebar__content-bg-dark'
           : 'custom-sidebar__content-bg-light'
       "
       class="custom-sidebar__content"
     >
       <div class="">
-        <picture class="is-flex">
-          <img
-            src="@/assets/img/googlelogo.png"
-            :alt="`$t('alt_image_logo')`"
-          />
-        </picture>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="mb-3 custom-sidebar__content-logo"
+        >
+          <picture class="is-flex ">
+            <img
+              src="@/assets/img/googlelogo.png"
+              :alt="`$t('alt_image_logo')`"
+            />
+          </picture>
+        </a>
         <div
           v-for="option in options"
           :key="option"
           class="custom-sidebar__content-item  is-flex is-align-items-center"
         >
-          <span class="material-icons">
-            {{ $t(`sidebar.option_${option}.icon`) }}
-          </span>
-          <p>{{ $t(`sidebar.option_${option}.text`) }}</p>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <span class="material-icons mr-3">
+              {{ $t(`sidebar.option_${option}.icon`) }}
+            </span>
+            <p>{{ $t(`sidebar.option_${option}.text`) }}</p>
+          </a>
         </div>
       </div>
-      <div class=""></div>
     </div>
   </div>
 </template>
 <script>
+import { mapState, mapMutations } from 'vuex'
+
 export default {
   name: 'CustomSidebar',
   data () {
     return {
-      isdark: false,
-      options: 11
+      options: 11,
+      isOpenSidebar: false
+    }
+  },
+  computed: { ...mapState(['backgroundDark', 'openSidebar']) },
+  methods: {
+    ...mapMutations(['SET_OPEN_SIDEBAR']),
+    touchSidebar () {
+      this.isOpenSidebar = !this.isOpenSidebar
+      this.SET_OPEN_SIDEBAR(this.isOpenSidebar)
     }
   }
 }
