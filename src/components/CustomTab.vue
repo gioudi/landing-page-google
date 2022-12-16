47006d3d633a5656a837bb38b0dc48e53249f322
Sergio Andres Penagos <sergio.penagos@vasslatam.com>
2022-12-15 22:17:21 -0500

    fix-nav

---
+++ src/components/CustomTab.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+    :class="
+        backgroundDark ? 'custom-tab__bg-dark' : 'custom-tab__bg-light'
+      "
+    >
+        <ul
+        :class="backgroundDark ? 'custom-tab__list-bg-dark' : 'custom-tab__list-bg-light'"
+        >
+            <li
+            v-for="(option, index) in options"
+            :key="index"
+            class="custom-tab__list-item"
+            @click="selectTab(option)"
+            :class="option.id === activeTab.id ? 'custom-tab__list-item-active' : ''" >
+                <p>{{option.text}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+  name: 'CustomTab',
+  computed: {
+    ...mapState(['backgroundDark', 'activeTab'])
+  },
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    ...mapMutations(['SET_TAB']),
+    selectTab (payload) {
+      this.SET_TAB(payload)
+    }
+  }
+}
+</script>
